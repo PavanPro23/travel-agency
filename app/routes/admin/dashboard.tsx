c53ef035c682a3c5acb5c97d9ecd605b48a9e67d
Pinninti Pavan <pinnintipavan04@gmail.com>
2025-07-24 12:11:13 +0530

    Implemented Dashboard Layout

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,8 +1,13 @@
+import Header from "components/Header"
+
 const dashboard = () => {
+    const user = {name: 'Sai'}
+
     return (
-        <div>
-            dashboard
-        </div>
+        <main className="dashboard wrapper">
+            <Header title={`Welcome ${user?.name ?? 'Guest'} 👋`} description="Track activity, trends and popular destinations in real time"/>
+            Dashboard Page Contents
+        </main>
     )
 }
 
